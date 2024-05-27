3c2e0782829b487782d4f2aebc522cfb81372cac
GustavoCSRamalho <gstvramalho@gmail.com>
2024-05-27 13:12:30 -0300

    adicionado retorna de erros

--- index.js
+++ index.js
@@ -8,30 +8,57 @@ server.use(express.json());
 // um método de encurtar uma URL persistindo-a no banco de dados.
 server.get("/shortUrl/:url", async (req, res) => {
     var url = req.params.url
-    var result = await insertUrlShorted(url)
-    return res.json(result);
+    await insertUrlShorted(url).then( result => {
+        if (result.length != 0) {
+            return res.json(result[0])
+        } else {
+            return res.status(404).send("URL nao encontrada!");
+        }
+    }).catch(error => {
+        return res.status(404).send("Aconteceu algum erro inesperado!");
+    })  
 });
 
 // um método que retorna uma url encurtada conforme o encurtamento da URL.
 server.get("/shortUrl/url/:value", async (req, res) => {
     var value = req.params.value
-    var result = await selectByUrl(value)
-    return res.json(result);
+    await selectByUrl(value).then( result => {
+        if (result.length != 0) {
+            return res.json(result[0])
+        } else {
+            return res.status(404).send("URL nao encontrada!");
+        }
+    }).catch(error => {
+        return res.status(404).send("Aconteceu algum erro inesperado!");
+    })
 });
 
 // um método que retorna uma url encurtada conforme um id.
 server.get("/shortUrl/id/:value", async (req, res) => {
     var value = req.params.value
-    var result = await selecById(value)
-    
-    return res.json(result);
+    await selecById(value).then( result => {
+        if (result.length != 0) {
+            return res.json(result[0])
+        } else {
+            return res.status(404).send("Id nao encontrado!");
+        }
+    }).catch(error => {
+        return res.status(404).send("Aconteceu algum erro inesperado!");
+    })    
 });
 
 // um método que retorna todas as URLs encurtadas em uma data específica.
 server.get("/shortUrl/all/:date", async (req, res) => {
     var date = req.params.date
-    var result = await selectAllUrlShortedFrom(date)
-    return res.json(result);
+    await selectAllUrlShortedFrom(date).then( result => {
+        if (result.length != 0) {
+            return res.json(result)
+        } else {
+            return res.status(404).send("Nada nessa data!");
+        }
+    }).catch(error => {
+        return res.status(404).send("Aconteceu algum erro inesperado!");
+    })  
 });
 
 server.listen(3333);
