6a1f49be4239fe44073240d9385f43a9ccfcfc7b
GustavoCSRamalho <gstvramalho@gmail.com>
2024-05-28 07:31:40 -0300

    ajustado banco de dados

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ async function connect(){
         return global.connection;
     }
     const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection({host: 'db',
+    const connection = await mysql.createConnection({host:'127.0.0.1',
     port: 3306,
     user: 'my-app-user',
     password: 'my-app-password',
